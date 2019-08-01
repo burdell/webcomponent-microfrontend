400f9d2ecded268ab8251b7ec4f7a2e7156362a2
Nathan Griffin <nathanjgriffin@gmail.com>
2019-08-01 18:26:46 -0400

    lol get outta here javascript

--- public/app.js
+++
@@ -1,2 +0,0 @@
-import { registerComponents } from "./web-components.js";
-registerComponents();
--- public/react-components.js
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-export function HelloReact() {
-    return React.createElement("div", null, "I'm a React component");
-}
