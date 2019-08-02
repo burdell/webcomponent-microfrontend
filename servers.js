f2e2a04a674d1dd4324c3e36bb50ebc4ba25c4c8
Nathan Griffin <nathanjgriffin@gmail.com>
2019-08-01 20:42:41 -0400

    adding files

---
+++ servers.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.static("./app/public"));
+app.get("/", function(req, res) {
+  res.sendFile(path.resolve(`${__dirname}/app/index.html`));
+});
+app.listen(4040);
+
+const frontends = express();
+frontends.get("/:frontend", function(req, res) {
+  res.sendFile(
+    path.resolve(`${__dirname}/frontends/public/${req.params.frontend}.js`)
+  );
+});
+
+frontends.listen(3030);
